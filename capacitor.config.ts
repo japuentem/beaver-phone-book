b827475a0cbd86db19b591d662ff9eb8fa08f286
alberto puente <japuentem@gmail.com>
2023-04-21 13:33:33 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'beaver-phone-book',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
